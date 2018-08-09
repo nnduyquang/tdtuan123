aa8b2f522f807a699708524350ed852387a9b808
loiPH <loiph.vncoder@users.noreply.github.com>
2018-08-09 10:34:41 +0700

    Up lại 4 file nha quang anh link lai roi.
    local\resources\views\frontend\common\contact_middle.blade.php
    local\resources\views\frontend\danhmuccv\index.blade.php
    local\resources\views\frontend\gioithieu\index.blade.php
    local\resources\views\frontend\home\index.blade.php

--- local/resources/views/frontend/home/index.blade.php
+++ local/resources/views/frontend/home/index.blade.php
@@ -18,60 +18,9 @@
 
 @stop
 @section('container')
-    <style>
-        #parallax_slogan {
-            background-image: url('{{URL::asset('images/bg/bg-home-recruit.png')}}');
-            -webkit-background-size: cover;
-            background-size: cover;
-            background-position: center center;
-            background-attachment: fixed;
-        }
 
-        #parallax_slogan h4 {
-            font-size: 26px;
-            color: white;
-            margin-bottom: 20px;
-            text-shadow: 1px 1px 2px #002752;
-            font-family: 'Lalezar', cursive;
-        }
 
-        #parallax_slogan button {
-            font-size: 20px;
-            color: white;
-            padding: 10px 20px;
-            border-radius: 3px;
-            border: none;
-            background: linear-gradient(-90deg, #0a6287, #0d95e8);
-        }
-    </style>
-
-    <div class="container-fluid main-font" id="parallax_slogan">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center pt-5 pb-5">
-                    <h4 class="pl-lg-5 pr-lg-5 p-sm-2 animated fadeInLeft">BẠN ĐANG KHAO KHÁT CỐNG HIẾN - HÃY LIÊN HỆ NGAY VỚI CHÚNG TÔI</h4>
-                    <div class="row wow bounceIn">
-                        <div class="col-md-4 mb-4">
-                            <button class="animated fadeIn" href="">
-                                <p>PHẠM THANH TUÂN<br>
-                                    0984 929 097 - 0908 074 446</p></button>
-                        </div>
-                        <div class="col-md-4 mb-4">
-                            <button class="animated fadeIn" href="">
-                                <p>ÂN ĐỨC NHÂN <br>
-                                    0906 037 688 - 0166 699 899</p></button>
-                        </div>
-                        <div class="col-md-4">
-                            <button class="animated fadeIn" href="">
-                                <p>TRẦN MINH TÙNG<br>
-                                    0962 383 611 - 0983 185 377</p></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
+    @include('frontend.common.contact_middle')
     {{--@include('frontend.home.NhanVienTieuBieu')--}}
     {{--@include('frontend.home.CamNghiNhanVien')--}}
     @include('frontend.common.ViTriKhac')
